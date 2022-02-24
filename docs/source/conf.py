ea8c99f51490d64d258267952913321e9ffa937c
Kreusada <kreusadaprojects@gmail.com>
2022-02-24 16:39:48 +0000

    fix docstrings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,9 +33,10 @@ release = oceanscript.__version__
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.extlinks",
-    "sphinx.ext.intersphinx",
+    'sphinx.ext.autodoc',
+    'sphinx.ext.extlinks',
+    'sphinx.ext.intersphinx',
+    'numpydoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
